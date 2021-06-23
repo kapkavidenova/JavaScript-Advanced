6a6a2a628862f7a6f129d4eb0808df32abdbb53e
kapkavidenova <76960502+kapkavidenova@users.noreply.github.com>
2021-06-23 15:06:00 +0300

    Delete DOM-Introduction-Lab directory

--- DOM-Introduction-Lab/04. JS-Advanced-DOM-Introduction-Lab-Resources (6)/02.Collect List Items/CollectListItems.js
+++
@@ -1,5 +0,0 @@
-function extractText() {
-  let itemsElements = document.getElementsByTagName('li');
-  let areaElements = [...itemsElements].map((el)=>el.textContent);
-  document.getElementById('result').value = areaElements.join('\n');
-}
--- DOM-Introduction-Lab/04. JS-Advanced-DOM-Introduction-Lab-Resources (6)/03.Sum Numbers/SumNumbers.js
+++
@@ -1,9 +0,0 @@
-function calc() {
-    // TODO: sum = num1 + num2
-    let first = Number(document.getElementById('num1').value);
-    let second = Number(document.getElementById('num2').value);
-    // let sum = first + second;
-    // document.getElementById('sum').value = sum;
-    let result = document.getElementById('sum').value;
-    result = first + second;
-}
