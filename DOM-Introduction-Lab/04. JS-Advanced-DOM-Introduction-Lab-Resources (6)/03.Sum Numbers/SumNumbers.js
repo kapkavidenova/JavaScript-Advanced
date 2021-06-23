f540165b2c11ab585b4bd4157b907c7bbe0e0d94
kapkavidenova <76960502+kapkavidenova@users.noreply.github.com>
2021-06-23 15:05:38 +0300

    Add files via upload

---
+++ DOM-Introduction-Lab/04. JS-Advanced-DOM-Introduction-Lab-Resources (6)/03.Sum Numbers/SumNumbers.js
@@ -0,0 +1,9 @@
+function calc() {
+    // TODO: sum = num1 + num2
+    let first = Number(document.getElementById('num1').value);
+    let second = Number(document.getElementById('num2').value);
+    // let sum = first + second;
+    // document.getElementById('sum').value = sum;
+    let result = document.getElementById('sum').value;
+    result = first + second;
+}
