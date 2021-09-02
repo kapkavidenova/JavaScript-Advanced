6ba6fd303d751d276d87c04d9370adbf4863b50f
kapkavidenova <76960502+kapkavidenova@users.noreply.github.com>
2021-09-02 16:37:06 +0300

    Add files via upload

---
+++ JS-Applications/ModularApplications/01.Team-Manager/src/api/data.js
@@ -0,0 +1,13 @@
+import * as api from './api.js';
+
+const host = 'http://localhost:3030';
+api.settings.host = host;
+
+export  const login = api.login;
+export  const register = api.register;
+export  const logout = api.logout;
+
+//Implement requests for our application
+
+
+
